4d9d4061babc4393f7d87a4fec17ff7ed16d9b29
omer9706 <omer_yurtseven1997@outlook.com>
2024-08-17 12:42:37 +0300

    .env file created for security

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import Dashboard from './Pages/Dashboard';
 import Login from './Pages/Login';
 import ProductDetails from './Pages/ProductDetails';
-import Registiration from './Pages/Registiration';
 import Navbar from './Components/Navbar';
 import ProtectedRoute from './Components/ProtectedRoute';
+import Registration from './Pages/Registiration';
 
 function App() {
-
   return (
     <Auth0Provider
-      domain="dev-n6z78tt75zdsfjt7.us.auth0.com"
-      clientId="pMp5njMxiKR3J8F8s1ir6aXGTHv9phpS"
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin, // Use window.location.origin for flexibility in environments
       }}
@@ -24,7 +23,8 @@ function App() {
           <Routes>
             <Route path="/" element={<ProtectedRoute component={Dashboard} />} />
             <Route path="/product-details/:id" element={<ProtectedRoute component={ProductDetails} />} />
-            <Route path="/registration" element={<ProtectedRoute component={Registiration} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
           </Routes>
         </Router>
       </div>
